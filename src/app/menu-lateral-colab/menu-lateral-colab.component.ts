b65e1e41f2426485da5f33612e8f95006eb6d90c
Rodrigo <rodrigomoraga45@gmail.com>
2020-07-07 02:33:42 -0400

    Cmabio en el menu de colab

--- src/app/menu-lateral-colab/menu-lateral-colab.component.ts
+++ src/app/menu-lateral-colab/menu-lateral-colab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-menu-lateral-colab',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuLateralColabComponent implements OnInit {
 
-  constructor() { }
+  element2:HTMLElement;
+  element3:HTMLElement;
+
+  constructor(private render: Renderer2) { }
 
   ngOnInit(): void {
+    this.element2 = document.getElementById('proyectos');
+    this.element3 = document.getElementById('desarrolladores');
+    this.render.addClass(this.element2,'active'); 
+  }
+
+  public activarItem(numero:number){
+    if(numero == 2){
+      this.render.addClass(this.element2,'active');
+      this.render.removeClass(this.element3,'active');
+    }else{
+      this.render.addClass(this.element3,'active');
+      this.render.removeClass(this.element2,'active');
+
+    }
   }
 
 }
